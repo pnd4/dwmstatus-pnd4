c552778099b205f8e86c56bb3fa5566bb07cc486
pnd4 <cfcomfreak@yahoo.com>
2013-10-05 01:59:51 -0700

    Removed getvolume.sh as it isn't necessary.

--- getvolume.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Option 1 : 3 processes
-#amixer sget Master,0 | egrep -o '([0-9]+%|\[(on|off)\])' | sed ':a;N;$!ba;s/\n/ /g'
-# Option 2 : 2 processes
-amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "Master Mute" } else { print $2"%" }}'
